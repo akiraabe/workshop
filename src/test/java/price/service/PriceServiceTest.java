097d23e4955e7168a53ba0f1508dd68eacb60308
akiraabe <akiraabe2005@yahoo.co.jp>
2019-03-18 19:46:47 +0900

    Add test case.

--- src/test/java/price/service/PriceServiceTest.java
+++ src/test/java/price/service/PriceServiceTest.java
@@ -14,7 +14,7 @@ import java.util.List;
 public class PriceServiceTest {
 
     @Test
-    public void testNormalSchenerio() {
+    public void testNormalSchenerioCountInBaby() {
 
         Party party = new Party();
         party.setFood (Food.needFood());
@@ -35,5 +35,37 @@ public class PriceServiceTest {
         assertThat(priceDetails.get(2).getAgeType()).isEqualTo(AgeType.CHILD);
         assertThat(priceDetails.get(2).getPrice().toString()).isEqualTo("16416.0");
 
+        assertThat(priceDetails.get(3).getAgeType()).isEqualTo(AgeType.BABY);
+        assertThat(priceDetails.get(3).getPrice().toString()).isEqualTo("3240");
+        assertThat(priceDetails.get(3).getBabyNeedFood()).isEqualTo(Boolean.FALSE);
+        assertThat(priceDetails.get(3).getBabyNeedBed()).isEqualTo(Boolean.TRUE);
     }
+    @Test
+    public void testNormalSchenerioCountWithoutBaby() {
+
+        Party party = new Party();
+        party.setFood (Food.needFood());
+        party.add(AgeType.ADULT,2 );
+        party.add(AgeType.CHILD, 1);
+        party.addBaby(1, Boolean.FALSE, Boolean.FALSE);
+
+        PriceService priceService = new PriceService(SeasonType.YELLOW, RoomType.SPECIAL, party);
+
+        List<PriceDetail> priceDetails = priceService.calculate();
+
+        // Print
+        priceDetails.forEach(System.out::println);
+
+        assertThat(priceDetails.get(0).getAgeType()).isEqualTo(AgeType.ADULT);
+        assertThat(priceDetails.get(0).getPrice().toString()).isEqualTo("21600");
+
+        assertThat(priceDetails.get(2).getAgeType()).isEqualTo(AgeType.CHILD);
+        assertThat(priceDetails.get(2).getPrice().toString()).isEqualTo("17280.0");
+
+        assertThat(priceDetails.get(3).getAgeType()).isEqualTo(AgeType.BABY);
+        assertThat(priceDetails.get(3).getPrice().toString()).isEqualTo("0");
+        assertThat(priceDetails.get(3).getBabyNeedFood()).isEqualTo(Boolean.FALSE);
+        assertThat(priceDetails.get(3).getBabyNeedBed()).isEqualTo(Boolean.FALSE);
+    }
+
 }
